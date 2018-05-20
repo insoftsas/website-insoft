9e83f1cfc947a2f200b5146171df32a92ca2833a
Sergio Molina <sergiomontiel1@hotmail.com>
2018-05-20 04:20:28 -0500

    Assigned roles seeder

--- database/seeds/AssignedRolesTableSeeder.php
+++ database/seeds/AssignedRolesTableSeeder.php
@@ -15,11 +15,8 @@ class AssignedRolesTableSeeder extends Seeder
 
             ['user_id' => 1, 'role_id' => 1, 'who_user' => 1],
             ['user_id' => 2, 'role_id' => 1, 'who_user' => 1],
-            ['user_id' => 3, 'role_id' => 3, 'who_user' => 1],
-            ['user_id' => 4, 'role_id' => 3, 'who_user' => 1],
-            ['user_id' => 5, 'role_id' => 3, 'who_user' => 1],
-            ['user_id' => 6, 'role_id' => 3, 'who_user' => 1],
-            ['user_id' => 7, 'role_id' => 3, 'who_user' => 1],
+            ['user_id' => 3, 'role_id' => 1, 'who_user' => 1],
+            ['user_id' => 4, 'role_id' => 1, 'who_user' => 1],
 
         ]);
     }
--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -4,6 +4,5 @@ const routes = [
     { path: '/', component: Index },
     { path: '*', redirect: '/' }
 ];
->>>>>>> bd2ab126cc56c025bb2075c0e679a34a9761d998
 
 export default routes;
