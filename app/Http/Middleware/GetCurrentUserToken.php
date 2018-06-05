cf6181bd42abe3bd030065e9b0ecc765b17a2b0c
Sergio Molina <sergiomontiel1@hotmail.com>
2018-06-05 16:59:22 -0500

    Fix getusertoken

--- app/Http/Middleware/GetCurrentUserToken.php
+++ app/Http/Middleware/GetCurrentUserToken.php
@@ -17,22 +17,26 @@ class GetCurrentUserToken
     public function handle($request, Closure $next)
     {
         if(auth()->guard("api")->check()){
-            $token = $request->user()->token();
-            $obj = (object) array(
-                'user_agent' => $request->header('user-agent')??'none',
-                'ip' => $request->ip(),
-                'time' => Carbon::now()->toDateTimeString(),
-                'url' => $request->server->get('REQUEST_URI')??'/',
-                'method' => $request->method()
-            );
-            $last = json_decode($token->last_access);
-            if($last != null){
-                if ( property_exists( $last , "locate") ) {
-                    $obj->locate = $last->locate;
+            if($request->user() != null){
+                if($request->user()->token() != null){
+                    $token = $request->user()->token();
+                    $obj = (object) array(
+                        'user_agent' => $request->header('user-agent')??'none',
+                        'ip' => $request->ip(),
+                        'time' => Carbon::now()->toDateTimeString(),
+                        'url' => $request->server->get('REQUEST_URI')??'/',
+                        'method' => $request->method()
+                    );
+                    $last = json_decode($token->last_access);
+                    if($last != null){
+                        if ( property_exists( $last , "locate") ) {
+                            $obj->locate = $last->locate;
+                        }
+                    }
+                    $token->last_access = json_encode($obj);
+                    $token->save();
                 }
             }
-            $token->last_access = json_encode($obj);
-            $token->save();
         }
         return $next($request);
     }
