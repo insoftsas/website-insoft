29448bbee2d94d46c9198e106733a2265187761c
Eduard Ramos <xseduard@gmail.com>
2018-09-04 21:48:03 -0500

    cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -43,6 +44,7 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
             \App\Http\Middleware\GetCurrentUserToken::class,
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
